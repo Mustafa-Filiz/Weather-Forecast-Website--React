a702536447f90e3d754553ed6164c5a9de80c7d1
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-05 17:26:46 +0300

    animated search button added

--- src/components/SearchArea.js
+++ src/components/SearchArea.js
@@ -1,22 +1,35 @@
 import React from 'react';
-import { Input, Button, Form } from 'semantic-ui-react';
+import { Input, Button, Form, Icon } from 'semantic-ui-react';
 
-const SearchArea = ({onFormSubmit, onButtonClick, city, inputEl, weatherInfo, onInputChange}) => {
+const SearchArea = ({
+    onFormSubmit,
+    onButtonClick,
+    city,
+    inputEl,
+    weatherInfo,
+    onInputChange,
+}) => {
     return (
         <Form className="input-form" onSubmit={onFormSubmit}>
-        <Button
-            size="large"
-            content="Your City =>"
-            onClick={onButtonClick}
-        />
-        <Input
-            size="large"
-            value={city}
-            ref={inputEl}
-            placeholder={weatherInfo?.location?.region}
-            onChange={onInputChange}
-        />
-    </Form>
+            <Button
+                size="large"
+                content="Your City =>"
+                onClick={onButtonClick}
+            />
+            <Input
+                size="large"
+                value={city}
+                ref={inputEl}
+                placeholder={weatherInfo?.location?.region}
+                onChange={onInputChange}
+            />
+            <Button animated type="submit" size="large">
+                <Button.Content visible>Search</Button.Content>
+                <Button.Content hidden>
+                    <Icon name="search" />
+                </Button.Content>
+            </Button>
+        </Form>
     );
 };
 
